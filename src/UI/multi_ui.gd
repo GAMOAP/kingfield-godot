e0c3d2c276515fadec21137430270ffe5e25bb77
axxwel <alexandregimeno@gamoap.com>
2025-04-25 08:44:22 +0200

    add start button

--- src/UI/multi_ui.gd
+++ src/UI/multi_ui.gd
@@ -8,11 +8,11 @@ var _last_valid_text := ""
 func _ready() -> void:
 	var result = await ServerConnection.authenticate_async()
 	if result == OK:
-		$connexionPanel/SubmitButton.disabled = false
+		$ConnexionPanel/SubmitButton.disabled = false
 		if not ServerConnection.is_new_session():
 			_userdata = await ServerConnection.get_user_account_async()
-			$connexionPanel/UsernameInput.text = _userdata.username
-			$connexionPanel/SubmitButton.text =  "Login"
+			$ConnexionPanel/UsernameInput.text = _userdata.username
+			$ConnexionPanel/SubmitButton.text =  "Login"
 
 func _on_username_input_text_changed(new_text: String) -> void:
 	var regex = RegEx.new()
@@ -20,16 +20,16 @@ func _on_username_input_text_changed(new_text: String) -> void:
 	
 	if regex.search(new_text):
 		_last_valid_text = new_text
-		$connexionPanel/UsernameInput.add_theme_color_override("font_color", Color.WHITE)
+		$ConnexionPanel/UsernameInput.add_theme_color_override("font_color", Color.WHITE)
 	else:
-		$connexionPanel/UsernameInput.text = _last_valid_text
-		$connexionPanel/UsernameInput.add_theme_color_override("font_color", Color.RED)
+		$ConnexionPanel/UsernameInput.text = _last_valid_text
+		$ConnexionPanel/UsernameInput.add_theme_color_override("font_color", Color.RED)
 
 func _on_username_input_text_submitted(input_text: String) -> void:
 	validate_username(input_text)
 
 func _on_submit_button_button_down() -> void:
-	var input_text = $connexionPanel/UsernameInput.text.strip_edges()  # Get the username without spaces
+	var input_text = $ConnexionPanel/UsernameInput.text.strip_edges()  # Get the username without spaces
 	validate_username(input_text)
 
 func validate_username(input_text: String) -> void:
@@ -37,7 +37,7 @@ func validate_username(input_text: String) -> void:
 	regex.compile("^[a-zA-Z0-9]{6,16}$")  # Min 6, max 12, alphanumerics only
 	
 	if not regex.search(input_text):
-		$connexionPanel/UsernameInput.add_theme_color_override("font_color", Color.RED)
+		$ConnexionPanel/UsernameInput.add_theme_color_override("font_color", Color.RED)
 	else:
 		user_connected(input_text)
 
@@ -46,7 +46,10 @@ func user_connected(username) -> void:
 			_userdata.username = username
 			ServerConnection.update_user_account_async(username)
 	
-	$connexionPanel.visible = false
+	$ConnexionPanel.visible = false
 	$UserPanel.visible = true
 	$UserPanel/UserName.text = username
+	
+	$ButtonStart.visible = true
+	
 	multi_UI_action.emit("user_connected")
